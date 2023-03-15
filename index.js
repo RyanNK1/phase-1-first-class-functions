fa446face046aa0a663974e5dc8a0ff67b6e8cfd
ryan <ryan.kimani0@gmail.com>
2023-03-15 13:05:59 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(chosen){
+    chosen()
+}
+function returnsANamedFunction(){
+    return function scoobydoo(){
+        
+    }
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+    }
+}
